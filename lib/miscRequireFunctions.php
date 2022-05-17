00d74d784d58b6c33b9499507f1972535dadd2a2
Mark Mathur <mm223jv@student.lnu.se>
2022-05-17 10:44:18 +0200

    Changed all DBhandler1_2 to just DBhandler.

--- lib/miscRequireFunctions.php
+++ lib/miscRequireFunctions.php
@@ -2,17 +2,17 @@
 
 // namespace DBhandler;
 
-  require_once './DBhandler1_2/STR.php';
-  require_once './DBhandler1_2/incomingDataClasses/Mother_targetPostWithOneCriteria.php';
+  require_once './DBhandler/STR.php';
+  require_once './DBhandler/incomingDataClasses/Mother_targetPostWithOneCriteria.php';
   require_once './ENV.php';
-  require_once './DBhandler1_2/lib/Extractor.php';
-  require_once './DBhandler1_2/lib/Unpacker.php';
-  require_once './DBhandler1_2/lib/StmtHandler.php';
-  require_once './DBhandler1_2/DBhandler.php';
+  require_once './DBhandler/lib/Extractor.php';
+  require_once './DBhandler/lib/Unpacker.php';
+  require_once './DBhandler/lib/StmtHandler.php';
+  require_once './DBhandler/DBhandler.php';
 
-  require_once './DBhandler1_2/incomingDataClasses/GetPostWithId.php';
-  require_once './DBhandler1_2/incomingDataClasses/GetPostsByCritera.php';
-  require_once './DBhandler1_2/incomingDataClasses/GetAllPosts.php';
-  require_once './DBhandler1_2/incomingDataClasses/UpdatePost.php';
-  require_once './DBhandler1_2/incomingDataClasses/StorePost.php';
-  require_once './DBhandler1_2/incomingDataClasses/DeletePostWithId.php';
+  require_once './DBhandler/incomingDataClasses/GetPostWithId.php';
+  require_once './DBhandler/incomingDataClasses/GetPostsByCritera.php';
+  require_once './DBhandler/incomingDataClasses/GetAllPosts.php';
+  require_once './DBhandler/incomingDataClasses/UpdatePost.php';
+  require_once './DBhandler/incomingDataClasses/StorePost.php';
+  require_once './DBhandler/incomingDataClasses/DeletePostWithId.php';
