e387b6b6b347146ab67881af3119d2046140d5c2
Mark Mathur <mathur.mark@gmail.com>
2023-07-21 11:01:22 +0200

    Added E_DEPRECATED because DBhandler needs to use dynamic properties. See StorePost.php

--- DBhandler.php
+++ DBhandler.php
@@ -5,7 +5,7 @@ namespace DBhandler;
 
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+error_reporting(E_ALL ^ E_DEPRECATED);
 
 // mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 mysqli_report(MYSQLI_REPORT_STRICT);
